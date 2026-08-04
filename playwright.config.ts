b1f2964250af58f70c72936e98c1594c181263ff
dee-git27 <deekshakarnika27@gmail.com>
2026-08-05 03:24:54 +0530

    add allure report step in config

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+              ['html'],
+              ['allure-playwright']
+            ],
   //reporter:'allure-playwright',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
