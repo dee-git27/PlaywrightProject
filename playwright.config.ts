1c88b29f3fabc8dd46e0ff96b09e8b02b0877ff1
dee-git27 <deekshakarnika27@gmail.com>
2026-08-19 23:57:21 +0530

    Fix configuration syntax errors in playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html,
+  reporter: 'html',
             'allure-playwright',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
