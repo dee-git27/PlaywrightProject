f1e6c8977d83bd88750a620f7fd1137e1c9b4f87
dee-git27 <deekshakarnika27@gmail.com>
2026-08-20 00:12:53 +0530

    Refactor reporter configuration in Playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,8 +26,10 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-            'allure-playwright',
+  reporter: [
+              ['html'],
+              ['allure-playwright']
+            ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
